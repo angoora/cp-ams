af08957fdde24de957a70a1ab870e94a84afed42
Kim Youngsan <consultant0392@gmail.com>
2022-04-12 23:26:40 +0900

    로그인 페이지 작업 중

--- common/js/common.js
+++ common/js/common.js
@@ -1,5 +1,5 @@
 //스크롤바 width 구하기
-function getScrollWidth(){
+function getScrollWidth() {
   const outer = document.createElement('div');
   outer.style.visibility = 'hidden';
   outer.style.overflow = 'scroll'; // forcing scrollbar to appear
@@ -20,14 +20,14 @@ function getScrollWidth(){
 
 //gnb 관련 함수
 function gnbAction() {
-  function openGnb(el){//gnb 펼치기
-    $('#container').stop().animate({marginLeft:'280px'},200);
-    $('.header').stop().animate({width:'280px'},200,function(){
+  function openGnb(el) {//gnb 펼치기
+    $('#container').stop().animate({ marginLeft: '280px' }, 200);
+    $('.header').stop().animate({ width: '280px' }, 200, function () {
       $(this).removeClass('sm');
       slideGnb(el);
-    });   
+    });
   }
-  function slideGnb(el){
+  function slideGnb(el) {
     var thisWrap = $(el).closest('li');
     if ($(el).hasClass('btn')) { //하위 메뉴 있는 경우 토글슬라이드
       $(el).next('.dep2').stop().slideToggle(200).closest('li').siblings('li').removeClass('active').find('.dep2').stop().slideUp(200);
@@ -48,14 +48,14 @@ function gnbAction() {
   });
 
   //dep1 클릭시 active 표기 하위메뉴 노출
-  $('#gnb .dep1 > li > a').on('click', function () {    
-    if($('.header').hasClass('sm')){
-      openGnb(this);      
-    }else{
+  $('#gnb .dep1 > li > a').on('click', function () {
+    if ($('.header').hasClass('sm')) {
+      openGnb(this);
+    } else {
       slideGnb(this);
     }
   });
- 
+
   //dep3 클릭시 active 표기
   $('.dep3 > li > a').on('click', function () {
     $('.dep3 > li').removeClass('active');
@@ -77,12 +77,12 @@ function gnbAction() {
   // });
 
   //gnb 감추기
-  $('.btn_menu').on('click',function(){
-    if($('.header').hasClass('sm')){//gnb펼치기
+  $('.btn_menu').on('click', function () {
+    if ($('.header').hasClass('sm')) {//gnb펼치기
       openGnb();
-    }else{//gnb줄이기
-      $('.header').addClass('sm').stop().animate({width:'80px'},200);
-      $('#container').stop().animate({marginLeft:'80px'},200);
+    } else {//gnb줄이기
+      $('.header').addClass('sm').stop().animate({ width: '80px' }, 200);
+      $('#container').stop().animate({ marginLeft: '80px' }, 200);
       $('#gnb .dep1 > li').removeClass('active');
       $('#gnb .dep2').stop().slideUp(200);
     }
@@ -90,29 +90,32 @@ function gnbAction() {
 }
 
 //팝업
-function modalPopup(){
-  $('[data-popopen]').click(function(){
+function modalPopup() {
+  $('[data-popopen]').click(function () {
     var popName = $(this).data('popopen');
-    $('[data-pop="'+popName+'"]').stop().fadeIn(200);
+    $('[data-pop="' + popName + '"]').stop().fadeIn(200);
   });
-  $('.pop-close').click(function(){
+  $('.pop-close').click(function () {
     $(this).closest('[data-pop]').fadeOut(200);
-  }); 
+  });
 
 }
 
 //페이징
-function pagination(){//pagination 클릭하면 active
-  $('.page-wrap a').click(function(){
+function pagination() {//pagination 클릭하면 active
+  $('.page-wrap a').click(function () {
     $(this).addClass('active').siblings('a').removeClass('active');
   });
 }
 
 //테이블 
-function fxTable(arg){
-  $('.table.fx-hd').each(function(){
-    $(this).find('table').clone().prependTo(this).addClass('clone-tb').attr('tabindex','-1').next('table').wrap('<div class="scr-y"></div>');
-    $(this).find('.clone-tb').css('width','calc(100% - '+arg+'px)');
+function fxTable(arg) {
+  $('.table.fx-hd').each(function () {
+    var theadH = $(this).find('thead').outerHeight();
+    var tdH = $(this).find('tbody td').outerHeight();
+    var maxHeight = $(this).data('row') * tdH + theadH;
+    $(this).find('table').clone().prependTo(this).addClass('clone-tb').attr('tabindex', '-1').next('table').wrap('<div class="scr-y" style="max-height:' + maxHeight + 'px"></div>');
+    $(this).find('.clone-tb').css('width', 'calc(100% - ' + arg + 'px)');
   });
 }
 
